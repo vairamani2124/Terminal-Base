75e3dde293dd813475e4277b1a112d696607de4d
vairamani2124 <vinimani2124@gmail.com>
2022-03-18 14:49:23 +0530

    initial commit

---
+++ JavaCode/src/javaPrograms/OddEvenNumber.java
@@ -0,0 +1,20 @@
+package javaPrograms;
+
+public class OddEvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=66;
+				
+		
+		if(a%2==0) {
+			System.out.println("this number is even");
+		}
+		else {
+			System.out.println("this number is not even");
+		}
+
+	}
+
+}
